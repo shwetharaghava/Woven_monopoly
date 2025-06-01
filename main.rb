4d9764eb58653e72f74280c8c8e10af95da57de2
“shwetha-raghava10” <swetz.bangera@gmail.com>
2025-06-01 20:58:36 +1000

    created main.rb to run the game

---
+++ main.rb
@@ -0,0 +1,25 @@
+require_relative 'lib/game'
+require 'json'
+
+puts "Welcome to Woven Monopoly!"
+puts " Choose a dice roll file : "
+puts "1. rolls_1.json"
+puts "2. rolls_2.json"
+puts "Enter 1 or 2 : "
+
+choice = gets.chomp
+
+roll_file = 
+  case choice
+  when "1"
+    "data/rolls_1.json"
+  when "2"
+    "data/rolls2.json"
+  else
+    puts "Invalid choice, selected 1.rolls_1.json"
+  end
+
+rolls = JSON.parse(File.read(roll_file))
+
+game = Game.new("data/board.json", rolls)
+game.play
